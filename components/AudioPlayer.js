f32056db3a1a0062dbe3cad37ecf6ee7ad9561cc
JamesLyddon <jameslyddon@gmail.com>
2022-04-18 15:07:17 +0100

    set up duration

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "../styles/AudioPlayer.module.css";
 import { TiArrowLeftOutline } from "react-icons/ti";
 import { TiArrowRightOutline } from "react-icons/ti";
@@ -8,14 +8,28 @@ import { TiMediaPauseOutline } from "react-icons/ti";
 const AudioPlayer = () => {
   // State
   const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
 
   // references
   const audioPlayer = useRef();
 
+  useEffect(() => {
+    const seconds = Math.floor(audioPlayer.current.duration);
+    setDuration(seconds);
+  }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
+
+  const calculateTimer = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    return `${returnedMinutes}:${returnedSeconds}`;
+  };
+
   const togglePlayPause = () => {
     const prevValue = isPlaying;
     setIsPlaying(!prevValue);
-
 
     if (!prevValue) {
       audioPlayer.current.play(); // from html DOM reference
@@ -52,7 +66,9 @@ const AudioPlayer = () => {
       </div>
 
       {/* duration */}
-      <div className={styles.duration}>1:49</div>
+      <div className={styles.duration}>
+        {duration && !isNaN(duration) && calculateTimer(duration)}
+      </div>
     </div>
   );
 };
